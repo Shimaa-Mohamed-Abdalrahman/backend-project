bd01166a7d6b332b6d141ae5bd11ec8a1cc09e65
Shimaa Mohamed <100611766+sha-ghaf@users.noreply.github.com>
2022-09-23 19:33:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req.url)
     if (req.url === '/') {
-        res.write('hello from home5')
+        res.write('hello from home')
     }else if (req.url === '/about') {
-        res.write('hello from about5')
+        res.write('hello from about')
     }
     res.end()
 })
-
 server.listen(3000)
