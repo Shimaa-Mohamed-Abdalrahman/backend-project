4322ad28ab9453c11cb6c6b615b910b50140fe33
Shimaa Mohamed <100611766+sha-ghaf@users.noreply.github.com>
2022-09-24 19:49:49 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,48 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log(req.url)
-    if (req.url === '/') {
-        res.write('hello from home')
-    }else if (req.url !== '/') {
-        var string = req.url
-        var arr = string.split('/')
-        res.write('hello from ' + arr[1].toString())
-    }
-    res.end()
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     console.log(req.url)
+//     if (req.url === '/') {
+//         res.write('hello from home')
+//     }else if (req.url !== '/') {
+//         var string = req.url
+//         var arr = string.split('/')
+//         res.write('hello from ' + arr[1].toString())
+//     }
+//     res.end()
+// })
+// server.listen(3000)
+
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+let users = []
+app.use(bodyParser.json());
+app.post("/register", (req, res) => {
+    const { firstName, lastName} = req.body;
+    users.push({firstName, lastName});
+    res.status(200).json({
+        message:"register successfully",
+        data: users,
+    })
 })
-server.listen(3000)
+app.post("/login", (req, res) => {
+    const { firstName, lastName} = req.body;
+    users.push({firstName, lastName});
+    if(users.includes({firstName, lastName})){
+        res.status(200).json({
+        message:"login successfully",
+    })
+    }else{
+        res.status(404).json({
+            message:"login is not successfully",
+        })
+    }
+    
+})
+app.listen(3000, (req , res) => {
+    console.log(`app listen at http://localhost:3000`);
+});
+
+
